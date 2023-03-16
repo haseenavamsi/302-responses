58ef12b132d1cc639fc5fb2d20afadb080c5d8e0
haseenavamsi <121420151+haseenavamsi@users.noreply.github.com>
2023-03-17 01:00:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-
+const fs = require('fs')
 const server = http.createServer((req, res) => {
   if (req.method === 'POST') {
     let body = '';
